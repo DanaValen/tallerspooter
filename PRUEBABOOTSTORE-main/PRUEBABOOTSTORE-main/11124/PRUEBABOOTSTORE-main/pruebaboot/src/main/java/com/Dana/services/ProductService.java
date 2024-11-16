81c04237645567adae7189e18be80c014069f484
DanaValen <128103873+DanaValen@users.noreply.github.com>
2024-11-15 20:02:05 -0500

    Update ProductService.java

--- PRUEBABOOTSTORE-main/PRUEBABOOTSTORE-main/11124/PRUEBABOOTSTORE-main/pruebaboot/src/main/java/com/Dana/services/ProductService.java
+++ PRUEBABOOTSTORE-main/PRUEBABOOTSTORE-main/11124/PRUEBABOOTSTORE-main/pruebaboot/src/main/java/com/Dana/services/ProductService.java
@@ -1,6 +1,6 @@
-package com.anyi.services;
+package com.Dana.services;
 
-import com.anyi.entity.ProductEntity;
+import com.Dana.entity.ProductEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
